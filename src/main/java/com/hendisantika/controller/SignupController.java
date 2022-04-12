734b5ebd592c044a5a6a9c1929934f4fb7b47409
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:51:15 +0700

    #43 - Add viewPage in SignupController

--- src/main/java/com/hendisantika/controller/SignupController.java
+++ src/main/java/com/hendisantika/controller/SignupController.java
@@ -4,6 +4,7 @@ import com.hendisantika.service.framework.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,5 +26,10 @@ public class SignupController {
     public SignupController(UserService userService, MessageSource messageSource) {
         this.userService = userService;
         this.messageSource = messageSource;
+    }
+
+    @GetMapping
+    public String viewPage() {
+        return "signup";
     }
 }
