ce930e300bb3ebcd3157c0c082eec4f8861c3e41
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:28:41 +0700

    #8 - Add WebConfig

---
+++ src/main/java/com/hendisantika/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.hendisantika.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/04/22
+ * Time: 08.28
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    private final MessageSource messageSource;
+    private final String[] RESOURCE_HANDLERS = {
+            "/webjars/**", "/css/**", "/js/**", "/img/**"
+    };
+    private final String[] RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/webjars/",
+            "classpath:/static/css/",
+            "classpath:/static/js/",
+            "classpath:/static/img/"
+    };
+
+    public WebConfig(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+}
