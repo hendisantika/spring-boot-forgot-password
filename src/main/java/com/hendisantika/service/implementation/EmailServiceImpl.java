544d8377e549a07a197a86842624652ddb3ab723
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:41:39 +0700

    #26 - Add send in EmailServiceImpl

--- src/main/java/com/hendisantika/service/implementation/EmailServiceImpl.java
+++ src/main/java/com/hendisantika/service/implementation/EmailServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hendisantika.service.implementation;
 
+import com.hendisantika.entity.Mail;
 import com.hendisantika.service.framework.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +33,23 @@ public class EmailServiceImpl implements EmailService {
         this.templateEngine = templateEngine;
     }
 
+    @Override
+    public void send(Mail mail) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
+            Context context = new Context();
+            context.setVariables(mail.getModel());
+            String html = templateEngine.process("email/email-template", context);
+            helper.setTo(mail.getTo());
+            helper.setFrom(mail.getFrom());
+            helper.setSubject(mail.getSubject());
+            helper.setText(html, true);
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
