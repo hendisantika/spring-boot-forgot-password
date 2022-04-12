caa49ddc9ff24a40394a6ff4b378952ab983c9b0
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:50:05 +0700

    #40 - Add viewPage in ResetPasswordController

--- src/main/java/com/hendisantika/controller/ResetPasswordController.java
+++ src/main/java/com/hendisantika/controller/ResetPasswordController.java
@@ -1,11 +1,18 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.PasswordResetToken;
 import com.hendisantika.service.framework.PasswordResetTokenService;
 import com.hendisantika.service.framework.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +36,19 @@ public class ResetPasswordController {
         this.tokenService = tokenService;
         this.messageSource = messageSource;
         this.userService = userService;
+    }
+
+    @GetMapping
+    public String viewPage(@RequestParam(name = "token", required = false) String token,
+                           Model model) {
+        PasswordResetToken passwordResetToken = tokenService.findByToken(token);
+        if (passwordResetToken == null) {
+            model.addAttribute("error", messageSource.getMessage("TOKEN_NOT_FOUND", new Object[]{}, Locale.ENGLISH));
+        } else if (passwordResetToken.getExpirationDate().isBefore(LocalDateTime.now())) {
+            model.addAttribute("error", messageSource.getMessage("TOKEN_EXPIRED", new Object[]{}, Locale.ENGLISH));
+        } else {
+            model.addAttribute("token", passwordResetToken.getToken());
+        }
+        return "reset-password";
     }
 }
