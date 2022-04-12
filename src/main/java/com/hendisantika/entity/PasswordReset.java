6c2fd15e97514b733e7f79fac045b0fd8bb3c61e
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:31:59 +0700

    #13 - Add PasswordReset Model

---
+++ src/main/java/com/hendisantika/entity/PasswordReset.java
@@ -0,0 +1,29 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/04/22
+ * Time: 08.31
+ */
+@Data
+@PasswordConfirmation(
+        password = "password",
+        confirmPassword = "confirmPassword",
+        message = "PASSWORDS_NOT_EQUAL"
+)
+public class PasswordReset {
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private String confirmPassword;
+    @NotEmpty
+    private String token;
+}
