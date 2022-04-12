25401574d377d606896131ab7340ccfbbac06e27
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:42:48 +0700

    #27 - Add PasswordResetTokenServiceImpl

---
+++ src/main/java/com/hendisantika/service/implementation/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hendisantika.service.implementation;
+
+import com.hendisantika.repository.PasswordResetTokenRepository;
+import com.hendisantika.service.framework.PasswordResetTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/04/22
+ * Time: 08.41
+ */
+@Service
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Autowired
+    public PasswordResetTokenServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+    }
+}
