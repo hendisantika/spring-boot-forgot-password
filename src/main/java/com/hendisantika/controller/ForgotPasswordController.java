1b9dfbb99d4c938c02b17f93d74b8eaf2b007a94
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:48:30 +0700

    #36 - Add viewPage in ForgotPasswordController

--- src/main/java/com/hendisantika/controller/ForgotPasswordController.java
+++ src/main/java/com/hendisantika/controller/ForgotPasswordController.java
@@ -6,6 +6,7 @@ import com.hendisantika.service.framework.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -33,4 +34,10 @@ public class ForgotPasswordController {
         this.passwordResetTokenService = passwordResetTokenService;
         this.emailService = emailService;
     }
+
+    @GetMapping
+    public String viewPage() {
+        return "forgot-password";
+    }
+
 }
