cd6a6943f283f9220ac4127e482d4ce27dd9ec9f
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:32:40 +0700

    #14 - Add PasswordConfirmation Validator

---
+++ src/main/java/com/hendisantika/validator/PasswordConfirmation.java
@@ -0,0 +1,38 @@
+package com.hendisantika.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/04/22
+ * Time: 08.32
+ */
+@Constraint(validatedBy = PasswordConfirmationValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordConfirmation {
+    String password();
+
+    String confirmPassword();
+
+    String message() default "Passwords must match!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        PasswordConfirmation[] value();
+    }
+}
