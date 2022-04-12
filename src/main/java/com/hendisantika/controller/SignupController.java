228f44d635945169693c7f055817a5d5c55bf8f4
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:51:45 +0700

    #44 - Add saveUser in SignupController

--- src/main/java/com/hendisantika/controller/SignupController.java
+++ src/main/java/com/hendisantika/controller/SignupController.java
@@ -1,11 +1,19 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.service.framework.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Locale;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +39,30 @@ public class SignupController {
     @GetMapping
     public String viewPage() {
         return "signup";
+    }
+
+    public String saveUser(@Valid @ModelAttribute("user") User user,
+                           BindingResult result,
+                           Model model,
+                           RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "signup";
+        }
+        if (userService.findByEmail(user.getEmail()) != null) {
+            model.addAttribute("error", messageSource.getMessage("EMAIL_EXISTS", new Object[]{}, Locale.ENGLISH));
+        }
+        user = userService.save(user);
+        if (user == null) {
+            model.addAttribute("error", messageSource.getMessage("EMAIL_NOT_SAVED", new Object[]{}, Locale.ENGLISH));
+            return "signup";
+        }
+        attributes.addFlashAttribute("success", messageSource.getMessage("EMAIL_SAVED", new Object[]{},
+                Locale.ENGLISH));
+        return "redirect:/signup";
+    }
+
+    @ModelAttribute("user")
+    public User user() {
+        return new User();
     }
 }
