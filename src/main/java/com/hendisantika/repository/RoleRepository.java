e6739789eb556cc5bc512a1244c2e1488a354e15
Hendi Santika <hendisantika@gmail.com>
2022-04-12 08:37:48 +0700

    #20 - Add RoleRepository

---
+++ src/main/java/com/hendisantika/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/04/22
+ * Time: 08.37
+ */
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
